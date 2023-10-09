72f4b55356d49a888cdcd1391bb7df025f4de77c
Esly Medeiros dos Santos <20221170150066@ifrn.local>
2023-10-09 14:47:17 -0300

    projeto2

---
+++ projeto2/Assets/scripts/aaaaaaaaa.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class aaaaaaaaa : MonoBehaviour
+{
+    public bool noChao;
+    public int velocidade = 100;
+    public int ForcaPulo = 7;
+    public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent(out rb);
+    }
+
+    void OnCollisionEnter(Collision col){
+        if (col.gameObject.tag == "chão"){
+            noChao = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputHorizontal = Input.GetAxisRaw("Horizontal");
+        float inputVertical = Input.GetAxisRaw("Vertical");
+
+        rb.AddForce(new Vector3(inputHorizontal, 0, inputVertical) * velocidade);
+
+        if (Input.GetKeyDown(KeyCode.Space) && noChao){
+            rb.AddForce(Vector3.up * ForcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+        
+        if (transform.position.y <= -30){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
